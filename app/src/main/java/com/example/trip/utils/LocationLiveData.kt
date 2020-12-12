3bc61bf4828a9e14866fb5bf7b6bce90871e9e5e
oognuyh <oognuyh@gmail.com>
2020-12-12 22:56:23 +0900

    add location live data

--- app/src/main/java/com/example/trip/utils/LocationLiveData.kt
+++ app/src/main/java/com/example/trip/utils/LocationLiveData.kt
@@ -4,21 +4,19 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
 import androidx.lifecycle.LiveData
-import com.example.trip.model.LocationInfo
+import com.example.trip.model.location.LocationInfo
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 
 class LocationLiveData(context: Context) : LiveData<LocationInfo>() {
-
     private var fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
     override fun onInactive() {
         super.onInactive()
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
-
 
     @SuppressLint("MissingPermission")
     override fun onActive() {
