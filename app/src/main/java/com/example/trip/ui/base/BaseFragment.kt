5bc907eeca25efac47a1ed218ca73374efac71c5
oognuyh <oognuyh@gmail.com>
2020-12-18 18:56:27 +0900

    change databinding to viewbinding because of tmap

--- app/src/main/java/com/example/trip/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/trip/ui/base/BaseFragment.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<B: ViewDataBinding>(
-    @LayoutRes
-    private val layoutId: Int
-) : Fragment() {
+abstract class BaseFragment<B: ViewBinding> : Fragment() {
     protected val TAG = javaClass.simpleName
     protected lateinit var binding: B
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
-        binding.lifecycleOwner = this
+        binding = getFragmentBinding(inflater, container)
 
         return binding.root
     }
+
+    abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): B
 }
