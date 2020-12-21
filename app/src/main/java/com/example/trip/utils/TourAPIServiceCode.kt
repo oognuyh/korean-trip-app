f17716f264d05b7c664ed51aaac86fd82e3489cf
oognuyh <oognuyh@gmail.com>
2020-12-21 22:39:16 +0900

    add tour api service code

---
+++ app/src/main/java/com/example/trip/utils/TourAPIServiceCode.kt
@@ -0,0 +1,21 @@
+package com.example.trip.utils
+
+object CONTENT_TYPE {
+    val TOURIST_ATTRACTION = "12"
+    val CULTURAL_FACILITIES = "14"
+    val FESTIVALS = "15"
+    val LEPORTS = "28"
+    val ACCOMMODATION = "32"
+    val SHOPPING = "38"
+    val DINING = "39"
+    val COURSE = "25"
+}
+
+object COURSE_TYPE {
+    val FAMILY = "C0112"
+    val ALONE = "C0113"
+    val HEALING = "C0114"
+    val WALKING = "C0115"
+    val CAMPING = "C0116"
+    val TASTE = "C0117"
+}
